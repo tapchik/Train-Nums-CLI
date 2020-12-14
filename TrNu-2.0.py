da9d4a592194c09c98d6147d696e9d58f0934720
Max <44063879+tapchik@users.noreply.github.com>
2020-12-14 13:31:52 +0500

    Figuring out tabs

--- TrNu-2.0.py
+++ TrNu-2.0.py
@@ -34,11 +34,11 @@ def script_settings():
 
     print('\n' + "Current max sum is: " + str(data["max sum"]))
     while True:
-try:
-	    x = int(input("Enter new max sum: "))
-	    return abs(x)
-	except ValueError:
-	    print('\n' + "Number is required, try again... " + '\n')
+        try:
+            x = int(input("Enter new max sum: "))
+            return abs(x)
+        except ValueError:
+            print('\n' + "Number is required, try again... " + '\n')
 
 def script_show_status():
     #displays settings and statistics
@@ -47,15 +47,15 @@ def script_show_status():
 
     print("Addition", end='    : ')
     if data["addition"] == True:
-	print("on")
+        print("on")
     else:
-	print("off")
+        print("off")
 
     print("Subtraction", end=' : ')
     if data["subtraction"] == True:
-	print("on")
+        print("on")
     else:
-	print("off")
+        print("off")
 
     total = data["correct"] + data["incorrect"] + data["skipped"]
 
@@ -66,68 +66,68 @@ def script_show_status():
     print("Solved problems total" + ' : ' + str(total))
 
     try:
-    print("{0}% of all problems are solved correctly".format(round(data["correct"]/total*100), 2))
+        print("{0}% of all problems are solved correctly".format(round(data["correct"]/total*100), 2))
     except ZeroDivisionError:
-    print("Further statistics are not available")
-    print('\n' + "Back to the problem... ")
+        print("Further statistics are not available")
+        print('\n' + "Back to the problem... ")
 
 #FUNCTIONS
 
 def file_read():
 
     try:
-    with open ("TrNuSettings.json", "r") as file:
-	x = json.load(file)
+        with open ("TrNuSettings.json", "r") as file:
+            x = json.load(file)
 
-	data["addition"] = x["addition"]
-	data["subtraction"] = x["subtraction"]
-	data["max sum"] = x["max sum"]
-	data["problem"] = x["problem"]
-	data["answer"] = x["answer"]
-	data["correct"] = x["correct"]
-	data["incorrect"] = x["incorrect"]
-	data["skipped"] = x["skipped"]
+            data["addition"] = x["addition"]
+            data["subtraction"] = x["subtraction"]
+            data["max sum"] = x["max sum"]
+            data["problem"] = x["problem"]
+            data["answer"] = x["answer"]
+            data["correct"] = x["correct"]
+            data["incorrect"] = x["incorrect"]
+            data["skipped"] = x["skipped"]
 
-	if (data["addition"] in [True, False]) and (data["subtraction"] in [True, False]) and (data["correct"] >= 0) and (data["incorrect"] >= 0) and (data["skipped"] >= 0) and (data["max sum"] > 0):
-	return True
-	else:
-	raise KeyError()
+            if (data["addition"] in [True, False]) and (data["subtraction"] in [True, False]) and (data["correct"] >= 0) and (data["incorrect"] >= 0) and (data["skipped"] >= 0) and (data["max sum"] > 0):
+                return True
+            else:
+                raise KeyError()
 
     except FileNotFoundError:
-    print ("This is a first startup of Train Nums on this computer. ")
-    print ("We love seeing new users! " + '\n')
-    return False
+        print ("This is a first startup of Train Nums on this computer. ")
+        print ("We love seeing new users! " + '\n')
+        return False
 
     except KeyError:
-    print ("Settings file must be currupted. Did you try to cheat? ")
-    print ("Program is set to default settings and your progress was nullified. " + '\n')
-    return False
+        print ("Settings file must be currupted. Did you try to cheat? ")
+        print ("Program is set to default settings and your progress was nullified. " + '\n')
+        return False
 
 def file_save():
-    with open ("TrNuSettings.json", "w") as file:
-    json.dump (data, file, indent = 4)
+    with open("TrNuSettings.json", "w") as file:
+        json.dump(data, file, indent = 4)
 
 def set_to_default():
 
     data = {"addition": True,
-    "subtraction": False,
-    "max sum": 25,
-    "current": [0, "+", 0, 0],
-    "correct": 0,
-    "incorrect": 0,
-    "skipped": 0}
+            "subtraction": False,
+            "max sum": 25,
+            "current": [0, "+", 0, 0],
+            "correct": 0,
+            "incorrect": 0,
+            "skipped": 0}
 
 def choose_operation():
 
     options = []
 
     if data["addition"] == True:
-    options.append("+")
+        options.append("+")
     if data["subtraction"] == True:
-    options.append("-")
+        options.append("-")
 
     if options != []:
-    return choice(options)
+        return choice(options)
     return None
 
 def generate_problem(operation):
@@ -140,28 +140,28 @@ def generate_problem(operation):
 
     if operation == '+':
 
-    answer = randint (1, data["max sum"])
-    left = randint (1, answer)
-    right = answer - left
+        answer = randint (1, data["max sum"])
+        left = randint (1, answer)
+        right = answer - left
 
-    problem += str(left)
-    problem += ' ' + operation + ' '
-    problem += str(right)
+        problem += str(left)
+        problem += ' ' + operation + ' '
+        problem += str(right)
 
     elif operation == '-':
 
-    left = randint (1, data["max sum"])
-    right = randint (1, left)
-    answer = left - right
+        left = randint (1, data["max sum"])
+        right = randint (1, left)
+        answer = left - right
 
-    problem += str(left)
-    problem += ' ' + operation + ' '
-    problem += str(right)
+        problem += str(left)
+        problem += ' ' + operation + ' '
+        problem += str(right)
 
     elif operation == None:
 
-    problem = None
-    answer = None
+        problem = None
+        answer = None
 
     return (problem, answer)
 
@@ -179,97 +179,96 @@ script_help()
 while True:
 
     if quitting == True:
-    break
+        break
 
     if solved == True:
-    operation = choose_operation()
-    data["problem"], data["answer"] = generate_problem(operation)
-    del operation
-    solved = False
+        operation = choose_operation()
+        data["problem"], data["answer"] = generate_problem(operation)
+        del operation
+        solved = False
 
     if data["problem"] is not None:
-    print ('\n' + data["problem"])
+        print ('\n' + data["problem"])
     else:
-    print ('\n' + "Generation is turned off. Choose an operation. ")
-    solved = True
+        print ('\n' + "Generation is turned off. Choose an operation. ")
+        solved = True
 
     user = input("User input: ")
 
     try:
-    user = int(user)
+        user = int(user)
     except ValueError:
-    user = str(user.lower())
+        user = str(user.lower())
 
-    if (user == data["answer"]):
-    print ("Correct!")
-    data["correct"] += 1
-    solved = True
+    if user == data["answer"]:
+        print ("Correct!")
+        data["correct"] += 1
+        solved = True
 
     elif isinstance(user, int):
-    print ('\n' + "Incorrect, try again")
-    data["incorrect"] += 1
+        print ('\n' + "Incorrect, try again")
+        data["incorrect"] += 1
 
     elif user in ["exit", "quit"]:
-    file_save()
-    quitting = True
+        file_save()
+        quitting = True
 
     elif user == "settings":
-    data["max sum"] = script_settings()
-    print ("Back to math problem... ")
+        data["max sum"] = script_settings()
+        print ("Back to math problem... ")
 
     elif user == "skip":
-    print ("We will count that. Next problem... ")
-    data["skipped"] += 1
-    solved = True
+        print ("We will count that. Next problem... ")
+        data["skipped"] += 1
+        solved = True
 
     elif user == "turn on addition":
-    if data["addition"] == True:
-	print ('\n' + "Addition is already turned on")
-    else:
-	print ('\n' + "Addition is turned on now")
-	data["addition"] = True
-    print ("Back to math problem... ")
+        if data["addition"] == True:
+            print ('\n' + "Addition is already turned on")
+        else:
+            print ('\n' + "Addition is turned on now")
+            data["addition"] = True
+        print ("Back to math problem... ")
 
     elif user == "turn off addition":
-    if data["addition"] == False:
-	print ('\n' + "Addition is already turned off")
-    else:
-	print ('\n' + "Addition is turned off now")
-	data["addition"] = False
-    print ("Back to math problem... ")
+        if data["addition"] == False:
+            print ('\n' + "Addition is already turned off")
+        else:
+            print ('\n' + "Addition is turned off now")
+            data["addition"] = False
+        print("Back to math problem... ")
 
     elif user == "turn on subtraction":
-    if data["subtraction"] == True:
-	print ('\n' + "Subtraction is already turned on")
-    else:
-	print ('\n' + "Subtraction is turned on now")
-	data["subtraction"] = True
-    print ("Back to math problem... ")
+        if data["subtraction"] == True:
+            print('\n' + "Subtraction is already turned on")
+        else:
+            print('\n' + "Subtraction is turned on now")
+            data["subtraction"] = True
+        print("Back to math problem... ")
 
     elif user == "turn off subtraction":
-    if data["subtraction"] == False:
-	print ('\n' + "Subtraction is already turned off")
-    else:
-	print ('\n' + "Subtraction is turned off now")
-	data["subtraction"] = False
-    print ("Back to math problem... ")
+        if data["subtraction"] == False:
+            print('\n' + "Subtraction is already turned off")
+        else:
+            print('\n' + "Subtraction is turned off now")
+            data["subtraction"] = False
+        print("Back to math problem... ")
 
     elif user == "delete progress":
-
-    data["correct"] = 0
-    data["incorrect"] = 0
-    data["skipped"] = 0
-    print ('\n' + "Your statistics were succesfully cleared... ")
+        data["correct"] = 0
+        data["incorrect"] = 0
+        data["skipped"] = 0
+        print('\n' + "Your statistics were succesfully cleared... ")
 
     elif user == "status":
-    script_show_status()
-    print('\n' + "Back to the math problem... ")
+        script_show_status()
+        print('\n' + "Back to the math problem... ")
 
     elif user == "help":
-    print (' ')
-    script_help()
+        print (' ')
+        script_help()
 
     else:
-    print ('\n' + "Unknown command, try again... ")
+        print ('\n' + "Unknown command, try again... ")
 
 input ('\n' + "End of program, press Enter to exit... ")
